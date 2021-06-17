857a72313e5fdf148c843f220057f18c23e664b5
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-06-17 22:45:27 +0200

    update calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Calculator from './Calculator/Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+      <div className="top">
+        <h1 className="title">Calculator.</h1>
+      </div>
+      <div className="center">
+        <Calculator />
+      </div>
+      <div className="bottom">
+        <span className="link">© {(new Date()).getFullYear()} Adrien N. F.</span>&nbsp;
+        <a href="/homepage" className="link">Back to Homepage</a>
+      </div>
     </div>
   );
 }
---
+++ src/Calculator/Calculator.js
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import "../App.css";
+
+const keys = [
+    [
+        '1',
+        '2',
+        '3',
+        '+'
+    ],
+    [
+        '4',
+        '5',
+        '6',
+        '-'
+    ],
+    [
+        '7',
+        '8',
+        '9',
+        '*'
+    ],
+    [
+        'C',
+        '0',
+        '=',
+        '/'
+    ],
+]
+
+export default function Calculator() {
+
+    const [calculatorText, setCalculatorText] = useState("WELCOME")
+
+    const pressKey = (key) => {
+        if (calculatorText === "WELCOME") {
+            setCalculatorText("");
+            return;
+        }
+        if (key === '=') {
+            pressedEqual();
+            return;
+        }
+        if (key === 'C') {
+            setCalculatorText("");
+            return;
+        }
+        let previousText = (calculatorText === "ERROR" || calculatorText === "WELCOME" ? "" : calculatorText);
+        setCalculatorText(previousText + ((previousText === "" || previousText === '0') && key === '0' ? "" : key));
+    }
+
+    const pressedEqual = () => {
+        let r = null;
+        try {
+            r = eval(calculatorText)
+            setCalculatorText(r && r.toString());
+        } catch {
+            setCalculatorText("ERROR")
+        }
+    }
+
+    return (
+        <div className="calculator-background">
+            <div className="calculator-screen">
+                <div className="calculator-screen-content">
+                    <span>{calculatorText && calculatorText.substr && calculatorText.substr(0, 11)}</span>
+                </div>
+            </div>
+            <div className="calculator-body">
+                {
+                    keys.map((line, i) => (
+                        <div className="calculator-line" key={i}>
+                            {
+                                line.map((key, j) => (
+                                    <input type="button" className="calculator-button" key={j} onClick={() => pressKey(key)} value={key} />
+                                ))
+                            }
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
